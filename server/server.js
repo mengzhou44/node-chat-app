34df734af17494fa7f8143ea48229e5f54285c61
meng zhou <mengzhou44@gmail.com>
2017-11-08 16:32:22 -0700

    application error!

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
