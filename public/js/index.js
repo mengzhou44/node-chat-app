4258829dc378bac50fdfa9b3c735a0906b8f0f2b
meng zhou <mengzhou44@gmail.com>
2017-11-09 13:01:02 -0700

    format timestamp using moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -17,16 +17,20 @@ socket.on('newUser', function(message) {
 });
 
 socket.on('newMessage', function(message) {
+  var formattedTime = moment().format('h:mm a');
+
   var li = $('<li></li>');
 
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
   $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
+  var formattedTime = moment().format('h:mm a');
+
   var li = $('<li></li>');
   var a = $(`<a target='_blank'> my current location </a>`);
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}:`);
   a.attr('href', message.url);
 
   li.append(a);
@@ -36,14 +40,16 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function(e) {
   e.preventDefault();
 
+  var messageTextbox = $('[name=message]');
+
   socket.emit(
     'createMessage',
     {
       from: 'User',
-      text: $('[name=message]').val()
+      text: messageTextbox.val()
     },
     function(message) {
-      console.log(message);
+      messageTextbox.val('');
     }
   );
 });
@@ -54,15 +60,17 @@ locationButton.on('click', function() {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser!');
   }
-
+  locationButton.attr('disabled', 'disabled').text('Sending location ...');
   navigator.geolocation.getCurrentPosition(
     function(position) {
+      locationButton.removeAttr('disabled').text('Send location');
       socket.emit('locationMessage', {
         longitude: position.coords.longitude,
         latitude: position.coords.latitude
       });
     },
     function(err) {
+      locationButton.removeAttr('disabled').text('Send location');
       alert('Unable to fecthc the location!');
     }
   );
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection', socket => {
 
   socket.on('createMessage', (message, callback) => {
     io.emit('newMessage', generateMessage(message.from, message.text));
-    callback('Server received this message.');
+    callback();
   });
 
   socket.on('locationMessage', location => {
